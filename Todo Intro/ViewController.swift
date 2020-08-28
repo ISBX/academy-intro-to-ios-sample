afe3808d3cf4f1f4e75cad71a294109b26f297b8
Tienshiao Ma <tma@isbx.com>
2020-08-27 20:12:40 -0700

    Initial Commit

---
+++ Todo Intro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo Intro
+//
+//  Created by Tienshiao Ma on 8/27/20.
+//  Copyright © 2020 ISBX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Todo IntroTests/Todo_IntroTests.swift
@@ -0,0 +1,34 @@
+//
+//  Todo_IntroTests.swift
+//  Todo IntroTests
+//
+//  Created by Tienshiao Ma on 8/27/20.
+//  Copyright © 2020 ISBX. All rights reserved.
+//
+
+import XCTest
+@testable import Todo_Intro
+
+class Todo_IntroTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
