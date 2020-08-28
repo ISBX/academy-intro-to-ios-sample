b9eec753f3122fe5bfee6e8138e9e9aec4cc9614
Tienshiao Ma <tma@isbx.com>
2020-08-27 22:40:59 -0700

    Add model, hardcode list, display in TableView

---
+++ Todo Intro/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  Todo Intro
+//
+//  Created by Tienshiao Ma on 8/27/20.
+//  Copyright © 2020 ISBX. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var description: String
+    var completed = false
+}
